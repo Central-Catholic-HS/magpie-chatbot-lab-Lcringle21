d2568e9d5ac1cf68f926d0c22439f7b8fd2ba095
Lcringle21 <72447998+Lcringle21@users.noreply.github.com>
2020-10-19 20:55:20 -0400

    Update Magpie4.java

--- Magpie4.java
+++ Magpie4.java
@@ -53,6 +53,11 @@ public class Magpie4
 		{
 			response = transformIWantToStatement(statement);
 		}
+		
+		else if (findKeyword(statement, "I want", 0) >= 0)
+		{
+		   response = iWantStatement(statement); 
+		  }
 
 		else
 		{
@@ -71,6 +76,22 @@ public class Magpie4
 			}
 		}
 		return response;
+	}
+	
+		private String iWantStatement(String statement)
+	{
+		//  Remove the final period, if there is one
+		statement = statement.trim();
+		String lastChar = statement.substring(statement
+				.length() - 1);
+		if (lastChar.equals("."))
+		{
+			statement = statement.substring(0, statement
+					.length() - 1);
+		}
+		int psn = findKeyword (statement, "I want", 0);
+		String restOfStatement = statement.substring(psn + 7).trim();
+		return "Would you really be happy if you had " + restOfStatement + "?";
 	}
 	
 	/**
